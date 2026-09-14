8c08c9b24b3d03686fd17312d11e209fbe4c525d
anq3l1 <apsereda08@gmail.com>
2026-09-14 16:46:36 +0300

    Добавить подавление вывода команд в configure.cpp для улучшения читаемости

--- src/configure.cpp
+++ src/configure.cpp
@@ -12,19 +12,19 @@ int configure_and_make(const std::string package)
     {
         std::cout << "Configurate...\n";
 
-        std::string command_tar = "tar -xvf " + file; 
+        std::string command_tar = "tar -xvf " + file + " > /dev/null 2>&1"; 
 
         system(command_tar.c_str());
 
         fs::path dir = parsingJsonNameAndVersion(package);
 
-        std::string command_conf = "cd \"" + dir.string() + "\" && ./configure";
+        std::string command_conf = "cd \"" + dir.string() + "\" && ./configure > /dev/null 2>&1";
 
         system(command_conf.c_str());
 
         std::cout << "Run: Make...\n";
 
-        std::string command_make = "cd \"" + dir.string() + "\" && make";
+        std::string command_make = "cd \"" + dir.string() + "\" && make > /dev/null 2>&1";
         
         system(command_make.c_str());
     }
